0291101f668679e534eef9af295f51fd4e838d53
Jennifer Feenstra-Arengard <sgjennifer@hotmail.com>
2024-09-15 15:48:30 +0200

    updating recipie site with API and styling

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export default function Card({recipe}) {
+export default function Card({ recipe }) {
   // the repice prop sent from App.js is recived and deconstructed for easy use in component
 
   // hit F12, or right click page and select "inspect" to open developer tools in the broswer. Select "console" and look at the recipe object
-  console.log(recipe) // prints to the console
+  console.log(recipe); // prints to the console
 
-  // TODO
-  // * complete the src-atribute on the img-tag to show the recipe picture
-  // * complete the href-attribute #top on the link a-tag to the proper recipe url
-  // * update the card with information you'd find interesting about the recipe
-  // * update the card styling
-  
   return (
-    <div className='recipe-card'>
-      <img src="" alt={recipe.label} />
+    <div className="recipe-card">
+      <img src={recipe.image} alt={recipe.label} />
       <h2>{recipe.label}</h2>
-      <a href="#top" className='button'>To recipe</a>
+      <h5>{recipe.cuisineType}</h5>
+      <a href={recipe.url} className="button">
+        To recipe
+      </a>
     </div>
-  )
+  );
 }
